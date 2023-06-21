d4a46b6db8b364bec98e3d75ded4c1e8530d631a
Abid Hussain <4838297+abidpu19@users.noreply.github.com>
2023-06-20 18:34:27 -0700

    Introduction of elsint use addition of .gitignore
    uses of internal  and external javascript files

---
+++ Introduction/eslint/index.js
@@ -0,0 +1,6 @@
+let fname = "abid";
+let lastname = "Hussain";
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+console.log(fname + lastname + " welcome ");
---
+++ Introduction/index.js
@@ -0,0 +1,1 @@
+console.log("Data is being loaded from external file ");
