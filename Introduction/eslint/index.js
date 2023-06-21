8578a93eee411644aff51bb37aa39c4326cd9a58
Abid Hussain <4838297+abidpu19@users.noreply.github.com>
2023-06-21 11:42:31 -0700

    Data Types in Javascript
    Started learning Data types in Javascript primitive and non primitive

--- Introduction/eslint/index.js
+++ Introduction/eslint/index.js
@@ -4,3 +4,12 @@ for (let i = 0; i <= 5; i++) {
   console.log(i);
 }
 console.log(fname + lastname + " welcome ");
+
+let radius = 23;
+// let radius = 34;
+radius = 34;
+var radius1 = 35;
+var radius1 = 45;
+radius1 = 65;
+const pi = 3.14159;
+// pi = 3.14;
